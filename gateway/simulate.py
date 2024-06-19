ad1ea72dbaf95ae3fc22214c4999d85711aab6f3
Felix <30195074+PhoenixClank@users.noreply.github.com>
2024-06-19 16:51:30 +0200

    add weather API access

--- gateway/simulate.py
+++ gateway/simulate.py
@@ -7,6 +7,38 @@ import random
 import time
 
 import pika
+import requests
+
+
+class ApiWeatherGovAccess:
+	"""A simple API client that accesses https://api.weather.gov"""
+
+	HEADERS = {'User-Agent': "study project of Felix Klenk, st149186@stud.uni-stuttgart.de"}
+
+	def __init__(self, type_id, instance_id, station_id, **kwargs):
+		self.type_id = type_id
+		self.instance_id = instance_id
+		self.url = "https://api.weather.gov/stations/{}/observations/latest".format(station_id)
+		self.cached_headers = None
+		self.cached_body = None
+		self.ttl = 0
+	
+	def sense(self):
+		time_curr = time.time_ns()
+		if self.ttl <= 0:
+			self.cached_response = requests.get(self.url, headers=ApiWeatherGovAccess.HEADERS)
+			self.cached_response.raise_for_status()
+			for directive in (s.strip() for s in self.cached_response.headers['Cache-Control'].split(',')):
+				if directive.startswith('max-age='):
+					self.ttl = int(directive[8:]) * 1_000_000_000
+					break
+		else:
+			self.ttl -= time_curr - self.time_prev
+		self.time_prev = time_curr
+		return {
+			'celsius': self.cached_response.json()['properties']['temperature']['value'],
+			'percent': self.cached_response.json()['properties']['relativeHumidity']['value']
+		}
 
 
 class NormallyDistributedDeltaSensor:
@@ -29,10 +61,11 @@ class NormallyDistributedDeltaSensor:
 	
 	def sense(self):
 		self.val_curr = max(self.val_min, min(self.val_curr + random.gauss(mu=0.0, sigma=self.sigma), self.val_max))
-		return self.val_curr
+		return {self.unit: self.val_curr}
 
 
 silly_dict = {  # Why doesn't __dict__ work?
+	'ApiWeatherGovAccess': ApiWeatherGovAccess,
 	'NormallyDistributedDeltaSensor': NormallyDistributedDeltaSensor,
 }
 
@@ -55,9 +88,7 @@ if __name__ == '__main__':
 					'timestamp': time.time_ns(),
 					'type_id': sensor.type_id,
 					'instance_id': sensor.instance_id,
-					'value': {
-						sensor.unit: sensor.sense(),
-					},
+					'value': sensor.sense(),
 				}))
 			time.sleep(config['poll_delay'])
 	except KeyboardInterrupt:
